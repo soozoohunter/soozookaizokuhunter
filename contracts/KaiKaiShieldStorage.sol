f3304d194cd2d71e9ecd568dd432f6219c87d89a
soozoohunter <zacyao88@icloud.com>
2025-03-29 14:46:49 +0800

    Update KaiKaiShieldStorage.sol

--- contracts/KaiKaiShieldStorage.sol
+++ contracts/KaiKaiShieldStorage.sol
@@ -2,6 +2,8 @@
 pragma solidity ^0.8.0;
 
 contract KaiKaiShieldStorage {
+    address public owner;
+
     struct Infringement {
         string infringingUrl;
         string evidence;
@@ -11,13 +13,27 @@ contract KaiKaiShieldStorage {
     mapping(bytes32 => bool) public storedFingerprint;
     mapping(bytes32 => Infringement[]) public infringements;
 
-    function storeFingerprint(bytes32 hash) public {
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function storeFingerprint(bytes32 hash) public onlyOwner {
         require(!storedFingerprint[hash], "Already stored!");
         storedFingerprint[hash] = true;
     }
 
-    function recordInfringement(bytes32 hash, string memory infringingUrl, string memory evidence) public {
+    function recordInfringement(bytes32 hash, string memory infringingUrl, string memory evidence) public onlyOwner {
         require(storedFingerprint[hash], "Fingerprint not found!");
-        infringements[hash].push(Infringement(infringingUrl, evidence, block.timestamp));
+        Infringement memory inf = Infringement({
+            infringingUrl: infringingUrl,
+            evidence: evidence,
+            timestamp: block.timestamp
+        });
+        infringements[hash].push(inf);
     }
 }
