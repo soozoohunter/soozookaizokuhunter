acbce7b210c04682c9da22cbf4a05e3723e41fe5
soozoohunter <zacyao88@icloud.com>
2025-03-29 14:36:52 +0800

    Update analytics.py

--- fastapi/analytics.py
+++ fastapi/analytics.py
@@ -3,9 +3,8 @@ import random
 def video_analysis(video_url: str):
     return {
         "video_url": video_url,
-        "views": random.randint(100, 100000),
+        "views": random.randint(100, 99999),
         "likes": random.randint(1, 5000),
-        "shares": random.randint(1, 2000),
         "CPI": round(random.uniform(0, 10), 2),
-        "CMA": "建議合作品牌 / 聯盟行銷"
+        "CMA": "廣告收益 / 聯盟行銷"
     }
