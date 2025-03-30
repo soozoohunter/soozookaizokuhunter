9c979fe099792b52395719ee990532887d905e8e
soozoohunter <zacyao88@icloud.com>
2025-03-30 12:39:43 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,9 +4,9 @@ import './App.css';
 function App() {
   return (
     <div style={{ textAlign: 'center', paddingTop: '10vh' }}>
-      <h1>Suzookaizokuhunter - V6</h1>
+      <h1>Suzookaizokuhunter - V7</h1>
       <h2>🔥 獵人出動，狩獵盜版無所遁形 🔥</h2>
-      <p>準備好與我們一起獵殺侵權者了嗎？</p>
+      <p>這裡可以上傳影片/圖片，並將指紋記錄到私有鏈！</p>
       <button className="btn-hunt">開始狩獵!</button>
     </div>
   );
