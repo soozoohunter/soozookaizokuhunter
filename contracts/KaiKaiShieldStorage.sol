94934e6cf48e02c39de838156d71ea96f86e7be6
soozoohunter <zacyao88@icloud.com>
2025-03-31 22:35:13 +0800

    Update KaiKaiShieldStorage.sol

--- contracts/KaiKaiShieldStorage.sol
+++ contracts/KaiKaiShieldStorage.sol
@@ -1,24 +1,21 @@
-// soozookaizokuhunter/contracts/KaiKaiShieldStorage.sol
-
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract KaiKaiShieldStorage {
     address public owner;
-
     mapping(bytes32 => bool) public storedFingerprint;
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Not authorized");
-        _;
-    }
 
     constructor() {
         owner = msg.sender;
     }
 
+    modifier onlyOwner {
+        require(msg.sender==owner,"Not authorized");
+        _;
+    }
+
     function storeFingerprint(bytes32 hash) public onlyOwner {
-        require(!storedFingerprint[hash], "Already stored!");
-        storedFingerprint[hash] = true;
+        require(!storedFingerprint[hash],"Already stored!");
+        storedFingerprint[hash]=true;
     }
 }
