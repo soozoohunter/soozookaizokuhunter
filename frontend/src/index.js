a3604c323bf03321a536040a98014e6118bc8f2b
soozoohunter <zacyao88@icloud.com>
2025-04-01 18:36:13 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,11 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './i18n/index.js'; // 初始化多語系
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
