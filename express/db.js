b56c6b14e68b5bec15ffa72ca5b8169c210520ec
soozoohunter <zacyao88@icloud.com>
2025-04-01 13:16:02 +0800

    Update db.js

--- express/db.js
+++ express/db.js
@@ -1,16 +1,17 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
+const {
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_DB,
+  POSTGRES_HOST,
+  POSTGRES_PORT
+} = process.env;
+
 const sequelize = new Sequelize(
-  process.env.POSTGRES_DB,
-  process.env.POSTGRES_USER,
-  process.env.POSTGRES_PASSWORD,
-  {
-    host: process.env.POSTGRES_HOST || 'db',
-    port: process.env.POSTGRES_PORT || 5432,
-    dialect: 'postgres',
-    logging: false
-  }
+  `postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}`,
+  { dialect:'postgres', logging:false }
 );
 
 module.exports = sequelize;
