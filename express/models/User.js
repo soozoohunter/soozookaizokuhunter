46c6a51d2c6f478e697807f0fd46d788285ac215
soozoohunter <zacyao88@icloud.com>
2025-04-01 13:16:51 +0800

    Update User.js

--- express/models/User.js
+++ express/models/User.js
@@ -1,11 +1,12 @@
 const bcrypt = require('bcrypt');
 
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    email: { type: DataTypes.STRING, unique: true },
-    passwordHash: DataTypes.STRING,
-    role: { type: DataTypes.ENUM('shortVideo','ecommerce'), defaultValue:'shortVideo' }
-  }, { tableName:'users' });
-
+module.exports = (sequelize, DataTypes)=>{
+  const User = sequelize.define('User',{
+    email:{ type:DataTypes.STRING, unique:true },
+    password_hash: DataTypes.STRING,
+    role: { type:DataTypes.STRING, defaultValue:'shortVideo' }
+  },{
+    tableName:'users'
+  });
   return User;
 };
