b64a98e97e8bbbc8cc92354216da852724b13016
soozoohunter <zacyao88@icloud.com>
2025-04-01 19:13:43 +0800

    Update User.js

--- express/models/User.js
+++ express/models/User.js
@@ -1,17 +1,25 @@
-module.exports = (sequelize, DataTypes) => {
-  const User = sequelize.define('User', {
-    email: {
-      type: DataTypes.STRING,
-      unique: true,
-      allowNull: false
-    },
-    password_hash: DataTypes.STRING,
-    role: {
-      type: DataTypes.STRING,
-      defaultValue: 'shortVideo'
-    }
-  },{
-    tableName: 'users'
-  });
-  return User;
-};
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const User = sequelize.define('User', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  email: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false
+  },
+  password_hash: DataTypes.STRING,
+  role: {
+    type: DataTypes.STRING,
+    defaultValue: 'shortVideo'
+  }
+}, {
+  tableName: 'users',
+  timestamps: false
+});
+
+module.exports = User;
