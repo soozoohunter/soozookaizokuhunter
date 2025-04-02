486e17bb2a3338fd8051083bfd6c3fbde19f9715
soozoohunter <zacyao88@icloud.com>
2025-04-02 14:37:24 +0800

    Update PlatformAccount.js

--- express/models/PlatformAccount.js
+++ express/models/PlatformAccount.js
@@ -1,8 +1,6 @@
-// express/models/PlatformAccount.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
 
-// 若要使用自動增號整數，可改成 id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true }
 const PlatformAccount = sequelize.define('PlatformAccount', {
   id: {
     type: DataTypes.UUID,
@@ -23,12 +21,18 @@ const PlatformAccount = sequelize.define('PlatformAccount', {
   },
   accessToken: {
     type: DataTypes.TEXT
+  },
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
+  },
+  updated_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
   }
 }, {
   tableName: 'platform_accounts',
-  timestamps: true,       // Sequelize會自動維護 createdAt, updatedAt
-  createdAt: 'created_at',
-  updatedAt: 'updated_at'
+  timestamps: false
 });
 
 module.exports = PlatformAccount;
