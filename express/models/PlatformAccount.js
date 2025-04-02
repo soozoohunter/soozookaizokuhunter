336e034c12540b5111f767921f244270a641c049
soozoohunter <zacyao88@icloud.com>
2025-04-02 17:02:52 +0800

    Update PlatformAccount.js

--- express/models/PlatformAccount.js
+++ express/models/PlatformAccount.js
@@ -2,37 +2,17 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
 
 const PlatformAccount = sequelize.define('PlatformAccount', {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true
-  },
-  platformName: {
+  platform: {
     type: DataTypes.STRING,
     allowNull: false
   },
-  accountId: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  isActive: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: true
-  },
-  accessToken: {
-    type: DataTypes.TEXT
-  },
-  created_at: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
-  },
-  updated_at: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW
+  account_details: {
+    type: DataTypes.JSONB,
+    allowNull: true
   }
 }, {
-  tableName: 'platform_accounts',
-  timestamps: false
+  tableName: 'PlatformAccounts',
+  timestamps: true
 });
 
 module.exports = PlatformAccount;
