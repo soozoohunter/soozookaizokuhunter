9258c5d2b34dcc0db715047d6709d8a03af10e9c
soozoohunter <zacyao88@icloud.com>
2025-04-02 14:48:45 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
-import './i18n/index.js'; // 初始化多語系
+import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import Dashboard from './pages/Dashboard';
+import PlatformAccounts from './pages/PlatformAccounts';
+import InfringementList from './pages/InfringementList';
+import UploadPage from './pages/UploadPage';
+import BlockchainTest from './pages/BlockchainTest';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="platform-accounts" element={<PlatformAccounts />} />
+        <Route path="infringements" element={<InfringementList />} />
+        <Route path="upload" element={<UploadPage />} />
+        <Route path="chain-test" element={<BlockchainTest />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
