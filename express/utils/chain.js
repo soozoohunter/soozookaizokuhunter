5a9fb0c803139927eef40176174bfaa8f455bee8
soozoohunter <zacyao88@icloud.com>
2025-04-02 18:30:57 +0800

    Update chain.js

--- express/utils/chain.js
+++ express/utils/chain.js
@@ -9,9 +9,17 @@ const contractABI = require('../../contracts/KaiKaiShieldStorage.abi.json');
 
 const contract = new web3.eth.Contract(contractABI, contractAddress);
 
-async function writeToBlockchain(data) {
+function getAccount() {
   const account = web3.eth.accounts.privateKeyToAccount(process.env.BLOCKCHAIN_PRIVATE_KEY);
   web3.eth.accounts.wallet.add(account);
+  return account;
+}
+
+/**
+ * 寫入最原始的 data
+ */
+async function writeToBlockchain(data) {
+  const account = getAccount();
   const tx = {
     from: account.address,
     to: contractAddress,
@@ -22,4 +30,41 @@ async function writeToBlockchain(data) {
   return receipt.transactionHash;
 }
 
-module.exports = { writeToBlockchain };
+/**
+ * 將使用者上傳檔案指紋 (DNAHash) 等資訊寫入鏈上
+ */
+async function writeUserAssetToChain(userEmail, dnaHash, fileType, timestamp) {
+  const account = getAccount();
+  // Demo: 將資料以 "USER:xxx|DNA:xxx|TYPE:xxx|TS:xxx" 形式一起存
+  const combined = `USER:${userEmail}|DNA:${dnaHash}|TYPE:${fileType}|TS:${timestamp}`;
+  const tx = {
+    from: account.address,
+    to: contractAddress,
+    data: contract.methods.storeData(combined).encodeABI(),
+    gas: 2000000
+  };
+  const receipt = await web3.eth.sendTransaction(tx);
+  return receipt.transactionHash;
+}
+
+/**
+ * 將侵權資訊寫入鏈上
+ */
+async function writeInfringementToChain(userEmail, infrInfo, timestamp) {
+  const account = getAccount();
+  const combined = `USER:${userEmail}|INFR:${infrInfo}|TS:${timestamp}`;
+  const tx = {
+    from: account.address,
+    to: contractAddress,
+    data: contract.methods.storeData(combined).encodeABI(),
+    gas: 2000000
+  };
+  const receipt = await web3.eth.sendTransaction(tx);
+  return receipt.transactionHash;
+}
+
+module.exports = {
+  writeToBlockchain,
+  writeUserAssetToChain,
+  writeInfringementToChain
+};
