1b5e6358250307636e277b6ef6f518c4a9c3526e
soozoohunter <zacyao88@icloud.com>
2025-04-03 18:58:53 +0800

    Update init.sh

--- geth/init.sh
+++ geth/init.sh
@@ -1,27 +1,21 @@
 #!/bin/sh
 set -e
 
-# 如果沒有 /data/geth/chaindata 則初始化
+# 如果 /data/geth/chaindata 不存在 => 第一次初始化
 if [ ! -d "/data/geth/chaindata" ]; then
   geth --datadir /data init /genesis.json
 fi
 
-# 假設您要用 <SignerAddr> 做出塊者
-# 需將該地址的 keystore JSON 放到 /data/keystore 或 unlock 方式
+# 此處填寫您的簽名者地址
+SIGNER="0x034f9688dE6Bf5709dA5C258b3825Cb01C5ae475"
 
-# 範例：預設先unlock signer
-# (可將密碼寫在 password.txt, 再 COPY password.txt /)
-# 本示例以 --allow-insecure-unlock + --unlock
-# 之後會自動產塊(無需 PoW)
 exec geth --datadir /data \
-  --http \
-  --http.addr 0.0.0.0 \
-  --http.port 8545 \
+  --networkid 15 \
+  --http --http.addr 0.0.0.0 --http.port 8545 \
   --http.api "eth,net,web3,personal" \
   --nodiscover \
-  --networkid 15 \
-  --unlock <SignerAddr> \
+  --unlock "$SIGNER" \
   --allow-insecure-unlock \
+  --keystore /data/keystore \
   --password /data/password.txt \
-  --syncmode full \
   --mine
