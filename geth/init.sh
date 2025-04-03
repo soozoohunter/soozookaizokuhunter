d537aa1b72727f50f104428feccd177851cbe565
soozoohunter <zacyao88@icloud.com>
2025-04-03 21:12:24 +0800

    Update init.sh

--- geth/init.sh
+++ geth/init.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
-set -e
+set -ex
 
-# 如果 /data/geth/chaindata 不存在 => 第一次初始化
+# 强制设置环境变量
+export SIGNER="0x034f9688dE6Bf5709dA5C258b3825Cb01C5ae475"
+
 if [ ! -d "/data/geth/chaindata" ]; then
+  echo "▄︻デ=====初始化以太坊私有鏈=====══━一"
   geth --datadir /data init /genesis.json
 fi
-
-# 此處填寫您的簽名者地址
-SIGNER="0x034f9688dE6Bf5709dA5C258b3825Cb01C5ae475"
 
 exec geth --datadir /data \
   --networkid 15 \
   --http --http.addr 0.0.0.0 --http.port 8545 \
-  --http.api "eth,net,web3,personal" \
+  --http.api "eth,net,web3,personal,miner" \
   --nodiscover \
-  --unlock "$SIGNER" \
   --allow-insecure-unlock \
-  --keystore /data/keystore \
+  --unlock "$SIGNER" \
   --password /data/password.txt \
-  --mine
+  --mine \
+  --miner.etherbase "$SIGNER" \
+  --miner.gasprice 0 \
+  --miner.gastarget 8000000
