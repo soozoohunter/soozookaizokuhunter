f8ddb4c5ea1ae4302f5b2a4a195943e25fb868b0
soozoohunter <zacyao88@icloud.com>
2025-04-03 15:03:01 +0800

    Update init.sh

--- geth/init.sh
+++ geth/init.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
+set -e
 
-# 第一次初始化私有鏈
-geth --datadir /data init /genesis.json
+# 如無 /data/geth/chaindata 則初始化
+if [ ! -d "/data/geth/chaindata" ]; then
+  geth --datadir /data init /genesis.json
+fi
 
-# 正式啟動 geth 節點
+# 正式啟動 + 自動挖礦
 exec geth --datadir /data \
   --http \
-  --http.addr "0.0.0.0" \
+  --http.addr 0.0.0.0 \
   --http.port 8545 \
   --http.api "eth,net,web3,personal" \
   --nodiscover \
-  --networkid 15
+  --networkid 15 \
+  --allow-insecure-unlock \
+  --mine \
+  --miner.threads 1
