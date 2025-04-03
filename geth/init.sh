915352e8dcaf235255e952fff3114fcadeaca9d8
soozoohunter <zacyao88@icloud.com>
2025-04-03 22:27:23 +0800

    Update init.sh

--- geth/init.sh
+++ geth/init.sh
@@ -1,21 +1,27 @@
 #!/bin/sh
 set -e
 
-# 如果 /data/geth/chaindata 不存在 => 第一次初始化
 if [ ! -d "/data/geth/chaindata" ]; then
   geth --datadir /data init /genesis.json
 fi
 
-# 2 個簽名者地址 (小寫)
-SIGNER1="0x034f9688de6bf5709da5c258b3825cb01c5ae475"
-SIGNER2="0xc8f98636ebb10dbcb216026db3dab527cf37c2ee"
+# 複製 keystore
+# 讓 geth 能找到 keystore 在 /data/keystore
+if [ -d "/data/keystore" ]; then
+  echo "keystore found..."
+fi
+
+# 假設 signer address = 0xABCD1234...
+# 請與 genesis.json allocate & extradata 相同
+SIGNER="0xABCD1234ABCDEFAAAAAA..."
 
 exec geth --datadir /data \
+  --syncmode 'full' \
   --networkid 15 \
   --http --http.addr 0.0.0.0 --http.port 8545 \
-  --http.api "eth,net,web3,personal" \
+  --http.api 'eth,net,web3,personal' \
   --nodiscover \
-  --unlock "$SIGNER1" --unlock "$SIGNER2" \
+  --unlock "$SIGNER" \
   --allow-insecure-unlock \
   --keystore /data/keystore \
   --password /data/password.txt \
