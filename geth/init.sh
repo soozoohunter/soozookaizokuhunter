3a50e92120f6dbb768718209331d16f86873de4d
soozoohunter <zacyao88@icloud.com>
2025-04-04 21:41:21 +0800

    Update init.sh

--- geth/init.sh
+++ geth/init.sh
@@ -1,25 +1,19 @@
 #!/bin/sh
 set -e
 
-echo "=== 列出容器內檔案 /data/keystore ==="
-ls -l /data/keystore
-
-echo "=== 顯示 password.txt ==="
-cat /data/password.txt
-
-echo "=== 初始化創世區塊 (若尚未初始化) ==="
 if [ ! -d "/data/geth/chaindata" ]; then
+  echo "初始化 genesis 區塊..."
   geth --datadir /data init /genesis.json
 fi
 
-echo "=== 解鎖兩個簽名者，開始 PoA 挖礦 ==="
+echo "啟動 Geth 節點並解鎖帳戶..."
 exec geth --datadir /data \
   --networkid 15 \
   --http --http.addr 0.0.0.0 --http.port 8545 \
   --http.api eth,net,web3,personal \
   --nodiscover \
   --allow-insecure-unlock \
-  --unlock "0x034f9688de6bf5709da5c258b3825cb01c5ae475,0xc8f98636ebb10dbcb216026db3dab527cf37c2ee" \
+  --unlock "0x75f4ba6a6ba55817324509bf73256e89a836e815" \
   --keystore /data/keystore \
   --password /data/password.txt \
   --mine
