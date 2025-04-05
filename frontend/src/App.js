b36907156a9f6e443af99111829cf4490bac5a61
soozoohunter <zacyao88@icloud.com>
2025-04-05 20:38:37 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,10 +6,18 @@ import RegisterPage from './pages/RegisterPage';
 import Dashboard from './pages/Dashboard';
 import UploadPage from './pages/UploadPage';
 import InfringementList from './pages/InfringementList';
+import logo from './logo1.png.heic'; // 引入您的 Logo
 
 function App() {
   return (
     <Router>
+      {/* Header 區域，您可以根據需求調整樣式 */}
+      <header style={{ display: 'flex', alignItems: 'center', padding: '1rem', borderBottom: '1px solid #ddd' }}>
+        <img src={logo} alt="Logo" style={{ width: '100px', marginRight: '1rem' }} />
+        <h1>您的應用程式名稱</h1>
+      </header>
+      
+      {/* 路由區域 */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
