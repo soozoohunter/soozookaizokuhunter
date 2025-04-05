daf679bf30a4ad8eeafa5e04bd30bf6bcf683e53
soozoohunter <zacyao88@icloud.com>
2025-04-05 14:40:33 +0000

    更新最新程式碼

--- geth/init.sh
+++ geth/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-# 若尚未初始化，執行創世區塊初始化
+# 若尚未初始化，則初始化創世區塊
 if [ ! -d "/geth/data/geth/chaindata" ]; then
   geth --datadir /geth/data init /geth/genesis.json
 fi
@@ -11,6 +11,6 @@ exec geth --datadir /geth/data --networkid 20250405 \
   --http --http.addr "0.0.0.0" --http.port 8545 \
   --http.api "db,eth,net,web3,personal,miner,clique" \
   --http.corsdomain "*" --allow-insecure-unlock \
-  --unlock "0x034f9688dE6Bf5709dA5C258b3825Cb01C5ae475" \
+  --unlock "0x034f9688de6bf5709da5c258b3825cb01c5ae475" \
   --password /geth/password.txt --mine --miner.gaslimit 4700000 \
-  --miner.etherbase "0x034f9688dE6Bf5709dA5C258b3825Cb01C5ae475"
+  --miner.etherbase "0x034f9688de6bf5709da5c258b3825cb01c5ae475"
