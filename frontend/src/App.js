2b484a38f0612831cde9ebc33d056151c1012a68
soozoohunter <zacyao88@icloud.com>
2025-04-05 17:28:54 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Pricing from './pages/Pricing';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import Dashboard from './pages/Dashboard';
-import PlatformAccounts from './pages/PlatformAccounts';
-import InfringementList from './pages/InfringementList';
 import UploadPage from './pages/UploadPage';
-import BlockchainTest from './pages/BlockchainTest';
+import InfringementList from './pages/InfringementList';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/platform-accounts" element={<PlatformAccounts />} />
-        <Route path="/infringements" element={<InfringementList />} />
         <Route path="/upload" element={<UploadPage />} />
-        <Route path="/blockchain-test" element={<BlockchainTest />} />
+        <Route path="/infringements" element={<InfringementList />} />
       </Routes>
     </Router>
   );
