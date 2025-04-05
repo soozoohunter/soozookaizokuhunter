987b7f953518a71bf580f9ddfd14e70a85c9cef6
soozoohunter <zacyao88@icloud.com>
2025-04-05 16:00:26 +0800

    Update init.sh

--- geth/init.sh
+++ geth/init.sh
@@ -10,10 +10,12 @@ echo "啟動 geth 並解鎖單一簽名者..."
 exec geth --datadir /data \
   --networkid 15 \
   --http --http.addr 0.0.0.0 --http.port 8545 \
-  --http.api eth,net,web3,personal \
+  --http.api eth,net,web3,personal,miner \
   --nodiscover \
   --allow-insecure-unlock \
   --unlock "0x1a65ee6db9d49bb286c17a3f705354601cd1f61b" \
   --keystore /data/keystore \
   --password /data/password.txt \
-  --mine
+  --mine \
+  --miner.etherbase "0x1a65ee6db9d49bb286c17a3f705354601cd1f61b" \
+  --miner.gasprice 0
