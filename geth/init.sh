055f433058f067630a3792b2f220a0e83a42ca24
soozoohunter <zacyao88@icloud.com>
2025-04-06 08:42:44 +0800

    Update init.sh

--- geth/init.sh
+++ geth/init.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 set -e
 
-# 若尚未初始化，則根據 genesis.json 初始化創世區塊
-if [ ! -d "/geth/data/geth/chaindata" ]; then
-  geth --datadir /geth/data init /geth/genesis.json
+DATA_DIR="/geth/data"
+
+if [ ! -d "$DATA_DIR/geth/chaindata" ]; then
+  echo "Initializing genesis block..."
+  geth --datadir "$DATA_DIR" init /geth/genesis.json
 fi
 
-# 啟動 Geth 私有鏈節點
-exec geth --datadir /geth/data --networkid 20250405 \
-  --http --http.addr "0.0.0.0" --http.port 8545 \
-  --http.api "db,eth,net,web3,personal,miner,clique" \
-  --http.corsdomain "*" --allow-insecure-unlock \
-  --unlock "0x034f9688de6bf5709da5c258b3825cb01c5ae475" \
-  --password /geth/password.txt --mine --miner.gaslimit 4700000 \
-  --miner.etherbase "0x034f9688de6bf5709da5c258b3825cb01c5ae475"
+echo "Starting Geth node..."
+exec geth --datadir "$DATA_DIR" --networkid 20250405 \
+  --http --http.addr "0.0.0.0" --http.port 8545 --http.api "db,eth,net,web3,personal,miner,clique" \
+  --nodiscover --allow-insecure-unlock \
+  --unlock "0x34f9688de6bf5709da5c258b3825cb01c5ae475" --password /geth/password.txt \
+  --mine --miner.gaslimit 4700000 --miner.etherbase "0x34f9688de6bf5709da5c258b3825cb01c5ae475"
