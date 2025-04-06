1ec21d3f2393bbf8d59adfc72210f073155bcbc2
soozoohunter <zacyao88@icloud.com>
2025-04-07 01:07:12 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';  // optional
+import './App.css';    // optional
+import './styles.css'; // optional
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
