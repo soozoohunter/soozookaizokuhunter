0341021b79a8f57b5c59b82ab981cfce6e33f7f1
soozoohunter <zacyao88@icloud.com>
2025-04-06 09:38:20 +0000

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,5 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// 建立 React root，並將 App 元件渲染到 id="root" 的 DOM 元素上
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
