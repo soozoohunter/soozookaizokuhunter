d915410105a9f52709b8b225490f4cc33e209e9e
soozoohunter <zacyao88@icloud.com>
2025-04-07 17:54:11 +0800

    Update db.js

--- express/db.js
+++ express/db.js
@@ -1,7 +1,13 @@
+// express/db.js
+
+const { Pool } = require('pg');
+
 const pool = new Pool({
   host: process.env.POSTGRES_HOST || 'postgres',
   port: process.env.POSTGRES_PORT || 5432,
-  user: process.env.POSTGRES_USER,         // suzoo
-  password: process.env.POSTGRES_PASSWORD, //KaiShieldDbPass2023!
-  database: process.env.POSTGRES_DB,       // suzoo_db
+  user: process.env.POSTGRES_USER,         // e.g. "suzoo"
+  password: process.env.POSTGRES_PASSWORD, // e.g. "KaiShieldDbPass2023!"
+  database: process.env.POSTGRES_DB,       // e.g. "suzoo_db"
 });
+
+module.exports = pool;
