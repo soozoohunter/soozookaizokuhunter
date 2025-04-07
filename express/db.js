614e793488d0794de755de659c50b620e194bf3a
soozoohunter <zacyao88@icloud.com>
2025-04-07 13:01:07 +0800

    Update db.js

--- express/db.js
+++ express/db.js
@@ -1,16 +1,7 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize(
-  process.env.POSTGRES_DB,
-  process.env.POSTGRES_USER,
-  process.env.POSTGRES_PASSWORD,
-  {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    dialect: 'postgres',
-    logging: false
-  }
-);
-
-module.exports = sequelize;
+const pool = new Pool({
+  host: process.env.POSTGRES_HOST || 'postgres',
+  port: process.env.POSTGRES_PORT || 5432,
+  user: process.env.POSTGRES_USER,         // suzoo
+  password: process.env.POSTGRES_PASSWORD, //KaiShieldDbPass2023!
+  database: process.env.POSTGRES_DB,       // suzoo_db
+});
