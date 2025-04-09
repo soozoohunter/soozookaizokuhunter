97d70c65149db69e2089e7aad71fbc7e363056b2
soozoohunter <zacyao88@icloud.com>
2025-04-09 20:33:59 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+
+// 匯入您剛剛修正的路由組態
 import MyRoutes from './MyRoutes';
 
+// 全域樣式 (若有)
 import './index.css';
 import './App.css';
 import './styles.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <BrowserRouter>
     <MyRoutes />
