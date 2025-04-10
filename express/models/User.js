326478160095cf149a2da3e57bec10326a9de44f
soozoohunter <zacyao88@icloud.com>
2025-04-10 21:32:41 +0800

    Update User.js

--- express/models/User.js
+++ express/models/User.js
@@ -1,25 +1,18 @@
 // express/models/User.js
-
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db'); 
 
 /**
- * User Model
- * email, password  -> 基本帳密
- * userName         -> 使用者暱稱
- * userRole         -> 'COPYRIGHT' or 'TRADEMARK' (也可 'BOTH')
- * platforms        -> JSON or TEXT, 儲存使用者平台帳號清單
- * trademarkLogo    -> 若該 userRole='TRADEMARK'，可在註冊時上傳商標圖, 這裡存檔案路徑/URL
- * registrationNo   -> 若該商標已核准, 可紀錄商標證號
+ * Users 資料表:
+ *   email, password, userName, userRole='COPYRIGHT|TRADEMARK|BOTH', 
+ *   platforms=TEXT(存JSON), trademarkLogo=STRING, registrationNo=STRING
  */
 const User = sequelize.define('User', {
   email: {
     type: DataTypes.STRING,
     allowNull: false,
     unique: true,
-    validate: {
-      isEmail: true
-    }
+    validate: { isEmail: true }
   },
   password: {
     type: DataTypes.STRING,
@@ -36,16 +29,14 @@ const User = sequelize.define('User', {
   },
   platforms: {
     type: DataTypes.TEXT, 
-    // or DataTypes.JSON
-    // 如果要儲存多組平台: "['instagram','facebook']"
     allowNull: true
   },
   trademarkLogo: {
-    type: DataTypes.STRING, // 儲存檔案路徑
+    type: DataTypes.STRING,
     allowNull: true
   },
   registrationNo: {
-    type: DataTypes.STRING, // 商標證號
+    type: DataTypes.STRING,
     allowNull: true
   }
 }, {
