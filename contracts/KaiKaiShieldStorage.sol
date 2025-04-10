bfdaf7402eda3791bec46d7e83853b6c5c9cd0de
soozoohunter <zacyao88@icloud.com>
2025-04-10 10:40:34 +0800

    Update KaiKaiShieldStorage.sol

--- contracts/KaiKaiShieldStorage.sol
+++ contracts/KaiKaiShieldStorage.sol
@@ -1,13 +1,20 @@
+// contracts/KaiKaiShieldStorage.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract KaiKaiShieldStorage {
-    string public data;
-    
+    // 簡單存一個字串
+    string public storedData;
+
     event DataStored(string data);
 
-    function storeData(string memory _data) public {
-        data = _data;
+    function storeData(string calldata _data) external {
+        storedData = _data;
         emit DataStored(_data);
+    }
+
+    // 讀取
+    function getData() external view returns (string memory) {
+        return storedData;
     }
 }
