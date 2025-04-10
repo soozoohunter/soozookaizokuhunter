5906154db83af075786b9be15cd1788e0caf0909
soozoohunter <zacyao88@icloud.com>
2025-04-10 20:55:19 +0800

    Update MyRoutes.js

--- frontend/src/MyRoutes.js
+++ frontend/src/MyRoutes.js
@@ -1,37 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
 import App from './App'; 
 import Home from './pages/Home';
 import Pricing from './pages/Pricing';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import DashboardPage from './pages/DashboardPage';
+import ProfilePage from './pages/ProfilePage';
 import UploadPage from './pages/UploadPage';
-import PlatformAccounts from './pages/PlatformAccounts';
-import InfringementList from './pages/InfringementList';
+import ContactUsPage from './pages/ContactUsPage';
 
 export default function MyRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* 最外層 path="/" → 使用 <App> 作為 Layout */}
         <Route path="/" element={<App />}>
-          {/* 首頁: "/" → index */}
           <Route index element={<Home />} />
-
-          {/* Pricing */}
           <Route path="pricing" element={<Pricing />} />
-
-          {/* Login / Register */}
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
-
-          {/* 其他頁面 */}
-          <Route path="dashboard" element={<DashboardPage />} />
+          <Route path="profile" element={<ProfilePage />} />
           <Route path="upload" element={<UploadPage />} />
-          <Route path="platform-accounts" element={<PlatformAccounts />} />
-          <Route path="infringements" element={<InfringementList />} />
+          <Route path="contact-us" element={<ContactUsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
