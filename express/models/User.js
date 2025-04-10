249a67c8ad032b027cda7a463b90135fe13d3ec7
soozoohunter <zacyao88@icloud.com>
2025-04-10 14:17:03 +0800

    Update User.js

--- express/models/User.js
+++ express/models/User.js
@@ -1,25 +1,26 @@
 // express/models/User.js
+
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db'); 
-// ↑ 確保此路徑正確，db.js 內已經連到 PostgreSQL
+// ↑ 假設 express/db.js 已經連到 PostgreSQL
 
+// 定義 'Users' 資料表
 const User = sequelize.define('User', {
   email: {
     type: DataTypes.STRING,
     allowNull: false,
-    unique: true,            // 不允許重複 Email
+    unique: true,  // 不允許重複
     validate: {
-      isEmail: true          // 若不是有效 Email => SequelizeValidationError
+      isEmail: true // 需符合 Email 格式
     }
   },
   password: {
     type: DataTypes.STRING,
     allowNull: false
   }
-  // 如果您想要 username, user_type 等，可自行加上
 }, {
-  tableName: 'Users', // 可自訂表名，或改為小寫
-  timestamps: true    // 是否需要 createdAt / updatedAt
+  tableName: 'Users',  // 或 'users'
+  timestamps: true     // 若需要 createdAt/updatedAt
 });
 
 module.exports = User;
