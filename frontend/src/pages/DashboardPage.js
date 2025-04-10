5d21d48d592663eb170da1b5430b3a06bfbbf198
soozoohunter <zacyao88@icloud.com>
2025-04-10 18:51:31 +0800

    Create DashboardPage.js

---
+++ frontend/src/pages/DashboardPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function DashboardPage() {
+  return (
+    <div style={{color:'#fff'}}>
+      <h2>Dashboard</h2>
+      <p>這裡是儀表板 (Dashboard) 頁面內容。</p>
+    </div>
+  );
+}
