5d07250ba2cc50bea578ea3e9772971366286de7
soozoohunter <zacyao88@icloud.com>
2025-04-10 19:22:56 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,24 +1,11 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
-import UploadPage from './pages/UploadPage';
-import DashboardPage from './pages/DashboardPage';
+import ReactDOM from 'react-dom/client';
+import MyRoutes from './MyRoutes';
+import './index.css'; // 若有全域CSS，可引用；沒有可省略
 
-export default function AppRoutes(){
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          {/* index => Home(在App Banner) */}
-          <Route path="login" element={<LoginPage />} />
-          <Route path="register" element={<RegisterPage />} />
-          <Route path="upload" element={<UploadPage />} />
-          <Route path="dashboard" element={<DashboardPage />} />
-          {/* etc. */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <MyRoutes />
+  </React.StrictMode>
+);
