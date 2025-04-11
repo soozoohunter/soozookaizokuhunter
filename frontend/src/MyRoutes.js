010505d1845e76e2b08fd1c9b857707d4fe9cb8c
soozoohunter <zacyao88@icloud.com>
2025-04-11 20:12:15 +0800

    Update MyRoutes.js

--- frontend/src/MyRoutes.js
+++ frontend/src/MyRoutes.js
@@ -1,6 +1,8 @@
+// frontend/src/MyRoutes.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import App from './App'; 
+import App from './App';
+
 import Home from './pages/Home';
 import Pricing from './pages/Pricing';
 import LoginPage from './pages/LoginPage';
@@ -13,14 +15,22 @@ export default function MyRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* App為Layout */}
         <Route path="/" element={<App />}>
+          {/* 首頁 */}
           <Route index element={<Home />} />
+
+          {/* Pricing / Contact Us */}
           <Route path="pricing" element={<Pricing />} />
+          <Route path="contact-us" element={<ContactUsPage />} />
+
+          {/* Auth */}
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
+
+          {/* Profile / Upload */}
           <Route path="profile" element={<ProfilePage />} />
           <Route path="upload" element={<UploadPage />} />
-          <Route path="contact-us" element={<ContactUsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
