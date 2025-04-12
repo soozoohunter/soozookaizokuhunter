ce6fc37fd62cfdca519cd288e2decdb8053c537d
soozoohunter <zacyao88@icloud.com>
2025-04-12 11:51:44 +0800

    Update db.js

--- express/db.js
+++ express/db.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  logging: false, 
+  logging: false
 });
+
 module.exports = sequelize;
