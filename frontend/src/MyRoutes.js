bc4377809ddeaf3227b179b61618b6ad3d4e1f84
soozoohunter <zacyao88@icloud.com>
2025-04-12 12:03:55 +0800

    Update MyRoutes.js

--- frontend/src/MyRoutes.js
+++ frontend/src/MyRoutes.js
@@ -2,20 +2,22 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 
-// 你的頁面
+// 頁面
 import Home from './pages/Home';
 import Pricing from './pages/Pricing';
 import ContactUsPage from './pages/ContactUsPage';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import ProfilePage from './pages/ProfilePage';
+import MembershipPage from './pages/MembershipPage';
 import UploadPage from './pages/UploadPage';
+import Dashboard from './pages/Dashboard';
+import PlatformAccounts from './pages/PlatformAccounts';
 
-export default function MyRoutes() {
+export default function MyRoutes(){
   return (
     <BrowserRouter>
       <Routes>
-        {/* 最外層 layout：App */}
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
           <Route path="pricing" element={<Pricing />} />
@@ -23,7 +25,10 @@ export default function MyRoutes() {
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
           <Route path="profile" element={<ProfilePage />} />
+          <Route path="membership" element={<MembershipPage />} />
+          <Route path="dashboard" element={<Dashboard />} />
           <Route path="upload" element={<UploadPage />} />
+          <Route path="platform-accounts" element={<PlatformAccounts token={localStorage.getItem('token')} />} />
         </Route>
       </Routes>
     </BrowserRouter>
