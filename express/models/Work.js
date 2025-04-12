eb2705d39ac4c718a48bd1012a4d2c4ecc1c969c
soozoohunter <zacyao88@icloud.com>
2025-04-12 19:11:18 +0800

    Update Work.js

--- express/models/Work.js
+++ express/models/Work.js
@@ -1,31 +1,13 @@
-// express/models/Work.js
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
 
 const Work = sequelize.define('Work', {
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
   title: {
     type: DataTypes.STRING,
     allowNull: false
   },
-  fileType: {
-    type: DataTypes.ENUM('shortVideo','image','trademark'),
-    defaultValue: 'image'
-  },
-  keywords: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  fingerprint: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  chainTx: {
-    type: DataTypes.STRING,
-    allowNull: true
+  description: {
+    type: DataTypes.TEXT
   }
 }, {
   tableName: 'Works',
