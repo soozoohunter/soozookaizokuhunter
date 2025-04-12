37ffd704ef2f2fe3150d8a49d46be415b84a81bc
soozoohunter <zacyao88@icloud.com>
2025-04-12 17:01:00 +0800

    Create HomePage.js

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div className="home">
+      <h1>SUZOO! KAIZOKU HUNTER SYSTEM</h1>
+    </div>
+  );
+}
+
+export default HomePage;
