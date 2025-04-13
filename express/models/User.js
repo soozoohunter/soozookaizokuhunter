684b85b4ddcf786b5c64240a0666b61e15098b7e
soozoohunter <zacyao88@icloud.com>
2025-04-13 23:30:07 +0800

    Update User.js

--- express/models/User.js
+++ express/models/User.js
@@ -1,52 +1,29 @@
-// 假設已經初始化 Sequelize 並連線，在此定義 User 模型
+// Sequelize 定義 User 模型
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
-      // 可加入 validate: { isEmail: true } 作為額外驗證
     },
     password: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    username: {
+    plan: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
+      defaultValue: 'basic'
     },
-    facebook: {
-      type: DataTypes.STRING,
-      allowNull: true
+    uploadVideos: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
     },
-    instagram: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    youtube: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    tiktok: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    shopee: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    ruten: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    amazon: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    taobao: {
-      type: DataTypes.STRING,
-      allowNull: true
+    uploadImages: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
     }
   });
   return User;
