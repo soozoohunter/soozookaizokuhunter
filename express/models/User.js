26d82300fa06f7f7915471c57d04f49be3726d43
soozoohunter <zacyao88@icloud.com>
2025-04-13 19:59:38 +0800

    Update User.js

--- express/models/User.js
+++ express/models/User.js
@@ -1,69 +1,53 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../db');
-
-const User = sequelize.define('User', {
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  userName: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  plan: {
-    type: DataTypes.ENUM('BASIC','PRO','ENTERPRISE'),
-    allowNull: false,
-    defaultValue: 'BASIC'
-  },
-  uploadVideos: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0
-  },
-  uploadImages: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    defaultValue: 0
-  },
-  isEmailVerified: {
-    type: DataTypes.BOOLEAN,
-    allowNull: false,
-    defaultValue: false
-  },
-
-  // 舊的 emailVerifyCode 你可留著或不用
-  emailVerifyCode: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  // 新增: 暫存驗證碼
-  tempEmailCode: {
-    type: DataTypes.STRING,
-    allowNull: true
-  },
-  tempEmailCodeExp: {
-    type: DataTypes.BIGINT,
-    allowNull: true
-  },
-
-  // 社群 / 電商
-  facebook:  { type: DataTypes.STRING, allowNull: true },
-  instagram: { type: DataTypes.STRING, allowNull: true },
-  youtube:   { type: DataTypes.STRING, allowNull: true },
-  tiktok:    { type: DataTypes.STRING, allowNull: true },
-  shopee:    { type: DataTypes.STRING, allowNull: true },
-  ruten:     { type: DataTypes.STRING, allowNull: true },
-  amazon:    { type: DataTypes.STRING, allowNull: true },
-  taobao:    { type: DataTypes.STRING, allowNull: true }
-
-}, {
-  tableName: 'Users',
-  timestamps: true
-});
-
-module.exports = User;
+// 假設已經初始化 Sequelize 並連線，在此定義 User 模型
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+      // 可加入 validate: { isEmail: true } 作為額外驗證
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    facebook: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    instagram: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    youtube: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    tiktok: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    shopee: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    ruten: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    amazon: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    taobao: {
+      type: DataTypes.STRING,
+      allowNull: true
+    }
+  });
+  return User;
+};
