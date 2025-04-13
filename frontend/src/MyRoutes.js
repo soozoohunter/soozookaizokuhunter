a7b2b312f813df8a92ae92f4e0d290d6a690b092
soozoohunter <zacyao88@icloud.com>
2025-04-13 18:30:32 +0800

    Update MyRoutes.js

--- frontend/src/MyRoutes.js
+++ frontend/src/MyRoutes.js
@@ -3,11 +3,10 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 
-// 頁面
 import Home from './pages/Home';
 import ContactUsPage from './pages/ContactUsPage';
 import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';    // <-- 新增
+import RegisterPage from './pages/RegisterPage'; 
 import ProfilePage from './pages/ProfilePage';
 import UploadPage from './pages/UploadPage';
 import Dashboard from './pages/Dashboard';
@@ -15,25 +14,20 @@ import MembershipPage from './pages/MembershipPage';
 import PlatformAccounts from './pages/PlatformAccounts';
 import PaymentPage from './pages/PaymentPage';
 import TrademarkCheckPage from './pages/TrademarkCheckPage';
-
-// PricingPage (英文版方案介紹)
-import PricingPage from './pages/PricingPage';       // <-- 新增
+import PricingPage from './pages/PricingPage';       
+import VerifyEmailPage from './pages/VerifyEmailPage';
 
 export default function MyRoutes(){
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          {/* 首頁 */}
           <Route index element={<Home />} />
-
-          {/* Pricing 頁 (英文介紹) */}
           <Route path="pricing" element={<PricingPage />} />
-
-          {/* 其他既有頁面 */}
           <Route path="contact-us" element={<ContactUsPage />} />
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} /> 
+          <Route path="verify-email" element={<VerifyEmailPage />} />
           <Route path="profile" element={<ProfilePage />} />
           <Route path="upload" element={<UploadPage />} />
           <Route path="dashboard" element={<Dashboard />} />
