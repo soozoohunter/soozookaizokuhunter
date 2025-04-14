962f1211102f1f6649e0f0b932a3ad147b98cc5b
soozoohunter <zacyao88@icloud.com>
2025-04-15 01:25:26 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,7 @@
-// frontend/src/index.js
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import MyRoutes from './MyRoutes';
-import './index.css'; // 若有自訂 CSS
+import './index.css';
 
-// 取得掛載點
-const rootElement = document.getElementById('root');
-
-// 建立 React Root 並渲染
-const root = createRoot(rootElement);
+const root = createRoot(document.getElementById('root'));
 root.render(<MyRoutes />);
