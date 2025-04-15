95e8dcced4f1d7968f336db86fa46ff2bc3e1187
soozoohunter <zacyao88@icloud.com>
2025-04-15 09:25:37 +0800

    Update User.js

--- express/models/User.js
+++ express/models/User.js
@@ -1,50 +1,73 @@
-/********************************************************************
- * models/User.js
- ********************************************************************/
-const { DataTypes } = require('sequelize');
-
-module.exports = (sequelize) => {
-  return sequelize.define('User', {
-    // email 改為 unique
+// models/User.js (Sequelize 模型定義)
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
     email: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
-    },
-    // bcryptjs 後的哈希
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
+      unique: true   // 電子郵件必須唯一
     },
     userName: {
-      field: 'user_name',
-      type: DataTypes.STRING,
-      allowNull: true
-    },
-    role: {
       type: DataTypes.STRING,
       allowNull: false,
-      defaultValue: 'copyright'
+      unique: true   // 使用者名稱必須唯一
     },
-    plan: {
+    password: {
       type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: 'BASIC'
+      allowNull: false             // 將存儲經過雜湊的密碼
     },
-    uploadVideos: {
-      field: 'upload_videos',
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      defaultValue: 0
+    // 社群/電商平台帳號欄位 (允許 null，但若填寫則必須唯一)
+    ig: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true                // IG 帳號唯一綁定
     },
-    uploadImages: {
-      field: 'upload_images',
-      type: DataTypes.INTEGER,
+    fb: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true                // FB 帳號唯一綁定
+    },
+    youtube: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true                // YouTube 帳號唯一綁定
+    },
+    tiktok: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true                // TikTok 帳號唯一綁定
+    },
+    shopee: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true                // Shopee 帳號唯一綁定
+    },
+    ruten: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true                // 露天拍賣 (Ruten) 帳號唯一綁定
+    },
+    ebay: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true                // eBay 帳號唯一綁定
+    },
+    amazon: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true                // Amazon 帳號唯一綁定
+    },
+    taobao: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: true                // 淘寶 (Taobao) 帳號唯一綁定
+    },
+    serialNumber: {
+      type: DataTypes.STRING,     // 序號格式 "YYYYMMNN"
       allowNull: false,
-      defaultValue: 0
+      unique: true                // 每個用戶有唯一序號
     }
   }, {
-    tableName: 'users',
-    timestamps: false
+    timestamps: true  // 使用 createdAt 紀錄註冊時間
   });
+  return User;
 };
