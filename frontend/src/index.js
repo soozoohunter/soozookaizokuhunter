27c2a210dc5d20f378c8ad83b3f47601596c61e2
soozoohunter <zacyao88@icloud.com>
2025-04-16 21:05:36 +0800

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,9 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-- import { BrowserRouter } from 'react-router-dom';
-- import App from './App';
-+ import MyRoutes from './MyRoutes';
+import MyRoutes from './MyRoutes';  // or './routes/MyRoutes' if that’s your real path
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--  <BrowserRouter>
--    <App />
--  </BrowserRouter>
-+  // 原本的程式碼註解保留
-+  /*
-+  <BrowserRouter>
-+    <App />
-+  </BrowserRouter>
-+  */
-
-+  <MyRoutes />
+  <MyRoutes />
 );
