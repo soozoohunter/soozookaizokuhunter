34edc6cf4553ab354a613150cf11da1361918ff1
soozoohunter <zacyao88@icloud.com>
2025-04-17 11:02:19 +0800

    Update User.js

--- express/models/User.js
+++ express/models/User.js
@@ -18,51 +18,59 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       unique: true
     },
-    // 加密後的密碼雜湊
+    // 密碼（哈希儲存）
     password: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    // 角色（預設'user'，如需其他角色可在此擴充）
+    // 角色：預設 'user'，可為 'admin' 做後台管理
     role: {
       type: DataTypes.STRING,
       allowNull: false,
       defaultValue: 'user'
     },
-    // 序號（允許為空）
+    // 會員方案 (plan)，預設 'free' 或 'BASIC'
+    plan: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'free'
+    },
+    // 序號（可選）
     serialNumber: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: true
+      unique: false // 若您需唯一可改 true，但您原先寫unique: true也行
     },
-    // 社群/電商帳號綁定資訊（允許為空）
+    // 社群/電商綁定資訊（可選）
     socialBinding: {
       type: DataTypes.STRING,
       allowNull: true
     },
-    // 會員方案(plan)欄位，如不需要可刪除
-    plan: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      defaultValue: 'BASIC'
+    // 是否已付款
+    isPaid: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
     }
   }, {
     // 模型選項
     hooks: {
-      // 在建立使用者前自動執行密碼加密
+      // 建立使用者前自動執行密碼加密
       beforeCreate: async (user, options) => {
         if (user.password) {
           const salt = await bcrypt.genSalt(10);
           user.password = await bcrypt.hash(user.password, salt);
         }
       },
-      // 如果有更新密碼，也可以在beforeUpdate中處理類似邏輯:
+      // 若有更新密碼需求，可在 beforeUpdate 中 similarly 雜湊
       // beforeUpdate: async (user, options) => { ... }
     },
-    // 如果使用下劃線命名，可以加上 underscored: true，看專案需要
+    // 如果要使用下劃線命名，可以加 underscored: true
+    // underscored: true,
   });
 
-  // 可定義模型關聯 (associations) 這裡略過，如: User.associate = models => { ... };
+  // 可定義模型關聯 (associations)
+  // 例如： User.associate = (models) => { ... };
 
   return User;
 };
