8e3cf9bf1f5262861140b3f0337d880e91152cbb
soozoohunter <zacyao88@icloud.com>
2025-04-17 21:27:48 +0800

    Update User.js

--- express/models/User.js
+++ express/models/User.js
@@ -9,65 +9,68 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       unique: true,
       validate: {
-        isEmail: true // 驗證格式為 Email
-      }
+        isEmail: true, // 驗證格式為 Email
+      },
     },
     // 用戶名
     userName: {
       type: DataTypes.STRING,
       allowNull: false,
-      unique: true
+      unique: true,
     },
     // 密碼（哈希儲存）
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     // 角色：預設 'user'，可為 'admin' 方便後台
     role: {
       type: DataTypes.STRING,
       allowNull: false,
-      defaultValue: 'user'
+      defaultValue: 'user',
     },
     // 會員方案 (plan)，預設 'free'
     plan: {
       type: DataTypes.STRING,
       allowNull: false,
-      defaultValue: 'free'
+      defaultValue: 'free',
     },
     // 序號 (optional)
     serialNumber: {
       type: DataTypes.STRING,
       allowNull: true,
-      unique: false // 若需唯一可改 true
+      unique: false, // 若需唯一可改 true
     },
-    // 社群/電商綁定資訊 (optional)
+    // 社群/電商綁定資訊 (optional) - 用字串儲存
     socialBinding: {
       type: DataTypes.STRING,
-      allowNull: true
+      allowNull: true,
     },
     // 是否已付款
     isPaid: {
       type: DataTypes.BOOLEAN,
       allowNull: false,
-      defaultValue: false
-    }
+      defaultValue: false,
+    },
   }, {
+    // Hooks
     hooks: {
       // 建立使用者前自動雜湊
-      beforeCreate: async (user) => {
+      beforeCreate: async (user, options) => {
         if (user.password) {
           const salt = await bcrypt.genSalt(10);
           user.password = await bcrypt.hash(user.password, salt);
         }
       },
-      // 若要在更新密碼時也加密，可在 beforeUpdate 中 similarly 處理
+      // 若要在更新密碼時雜湊，可再加 beforeUpdate
       // beforeUpdate: async (user, options) => { ... }
-    }
-    // underscored: true, // 如果要使用下劃線
+    },
+    // 其他 Model 設定
+    // tableName: 'users', // 若要自訂資料表名稱
+    // underscored: true,  // 若要使用下劃線
   });
 
-  // 可定義關聯
+  // 可在此定義關聯
   // User.associate = models => { ... };
 
   return User;
