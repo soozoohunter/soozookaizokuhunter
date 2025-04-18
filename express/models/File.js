067aa0ead250908b693e77f81e024243b1bfb61f
soozoohunter <zacyao88@icloud.com>
2025-04-18 08:02:08 +0000

    Update File.js

--- express/models/File.js
+++ express/models/File.js
@@ -1,55 +1,31 @@
 'use strict';
+
 module.exports = (sequelize, DataTypes) => {
   const File = sequelize.define('File', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
+    filename: { type: DataTypes.STRING, allowNull: false },
+    fingerprint: { type: DataTypes.STRING(64) },
+    ipfs_hash: { type: DataTypes.TEXT },
+    cloud_url: { type: DataTypes.TEXT },
+    dmca_flag: { type: DataTypes.BOOLEAN, defaultValue: false },
+    tx_hash: { type: DataTypes.STRING(66) },
+    uploaded_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
     user_id: {
       type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    filename: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    fingerprint: {
-      type: DataTypes.STRING(64),
-      allowNull: true
-    },
-    ipfs_hash: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    cloud_url: {
-      type: DataTypes.TEXT,
-      allowNull: true
-    },
-    dmca_flag: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false
-    },
-    tx_hash: {
-      type: DataTypes.STRING(66),
-      allowNull: true
-    },
-    uploaded_at: {
-      type: DataTypes.DATE,
-      defaultValue: DataTypes.NOW
+      allowNull: false,
+      references: {
+        model: 'Users',
+        key: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
     }
   }, {
-    tableName: 'Files',
+    tableName: 'files',
     timestamps: false
   });
 
   File.associate = models => {
-    File.belongsTo(models.User, {
-      as: 'owner',
-      foreignKey: 'user_id',
-      onDelete: 'CASCADE',
-      onUpdate: 'CASCADE'
-    });
+    File.belongsTo(models.User, { as: 'owner', foreignKey: 'user_id' });
   };
 
   return File;
