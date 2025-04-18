757d55ab1c6d0a03e8ff87fa569c93658fedc6e7
soozoohunter <zacyao88@icloud.com>
2025-04-19 00:17:34 +0800

    Update File.js

--- express/models/File.js
+++ express/models/File.js
@@ -2,18 +2,45 @@
 
 module.exports = (sequelize, DataTypes) => {
   const File = sequelize.define('File', {
-    filename: { type: DataTypes.STRING, allowNull: false },
-    fingerprint: { type: DataTypes.STRING(64) },
-    ipfs_hash: { type: DataTypes.TEXT },
-    cloud_url: { type: DataTypes.TEXT },
-    dmca_flag: { type: DataTypes.BOOLEAN, defaultValue: false },
-    tx_hash: { type: DataTypes.STRING(66) },
-    uploaded_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    // ★ 將原先的 filename 與 snippet 的 fileName 統一為 fileName
+    fileName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    // ★ 新增 filePath 欄位
+    filePath: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    // ★ 從原先 snippet 保留
+    fingerprint: {
+      type: DataTypes.STRING(64)
+    },
+    ipfs_hash: {
+      type: DataTypes.TEXT
+    },
+    cloud_url: {
+      type: DataTypes.TEXT
+    },
+    dmca_flag: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    tx_hash: {
+      type: DataTypes.STRING(66)
+    },
+    uploaded_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
+
+    // ★ 關聯到 user_id
     user_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       references: {
-        model: 'Users',
+        model: 'Users',  // 注意：這裡參考的是資料庫內的 "Users" 表
         key: 'id'
       },
       onUpdate: 'CASCADE',
@@ -24,6 +51,7 @@ module.exports = (sequelize, DataTypes) => {
     timestamps: false
   });
 
+  // 關聯設定
   File.associate = models => {
     File.belongsTo(models.User, { as: 'owner', foreignKey: 'user_id' });
   };
