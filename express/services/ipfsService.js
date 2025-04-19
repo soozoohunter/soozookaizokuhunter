69ca8d91a772104ef1973284610dfc730208fba2
soozoohunter <zacyao88@icloud.com>
2025-04-19 10:54:59 +0800

    Update ipfsService.js

--- express/services/ipfsService.js
+++ express/services/ipfsService.js
@@ -4,6 +4,7 @@
 const { create } = require('ipfs-http-client');
 
 let ipfs = null;
+
 try {
   // 透過環境變數指定 IPFS 節點
   const ipfsHost = process.env.IPFS_HOST || 'suzoo_ipfs';
@@ -17,7 +18,6 @@ try {
 async function saveFile(buffer) {
   if (!ipfs) throw new Error('IPFS 未初始化');
   const result = await ipfs.add(buffer);
-  // `result` 可能包含 path 或 cid 屬性
   const cid = result.cid.toString();
   return cid;
 }
