507a5a80fe69c4358d4936ace2fb21e92b8e0a3e
soozoohunter <zacyao88@icloud.com>
2025-04-19 08:28:42 +0800

    Update File.js

--- express/models/File.js
+++ express/models/File.js
@@ -2,18 +2,41 @@
 
 module.exports = (sequelize, DataTypes) => {
   const File = sequelize.define('File', {
-    fileName: { type: DataTypes.STRING, allowNull: false },
-    filePath: { type: DataTypes.STRING, allowNull: false },
-    fingerprint: { type: DataTypes.STRING(64) },
-    ipfs_hash: { type: DataTypes.TEXT },
-    cloud_url: { type: DataTypes.TEXT },
-    dmca_flag: { type: DataTypes.BOOLEAN, defaultValue: false },
-    tx_hash: { type: DataTypes.STRING(66) },
-    uploaded_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    fileName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    filePath: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    fingerprint: {
+      type: DataTypes.STRING(64)
+    },
+    ipfs_hash: {
+      type: DataTypes.TEXT
+    },
+    cloud_url: {
+      type: DataTypes.TEXT
+    },
+    dmca_flag: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    tx_hash: {
+      type: DataTypes.STRING(66)
+    },
+    uploaded_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW
+    },
     user_id: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      references: { model: 'users', key: 'id' },
+      references: {
+        model: 'users',  // DB裡的 users 表
+        key: 'id'
+      },
       onUpdate: 'CASCADE',
       onDelete: 'CASCADE'
     }
@@ -23,6 +46,7 @@ module.exports = (sequelize, DataTypes) => {
   });
 
   File.associate = (models) => {
+    // 若 models.User 存在
     File.belongsTo(models.User, { as: 'owner', foreignKey: 'user_id' });
   };
 
