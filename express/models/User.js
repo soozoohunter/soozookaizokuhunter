751d6a6990c307c750047da8b807a779ec66cb55
soozoohunter <zacyao88@icloud.com>
2025-04-21 00:35:09 +0800

    Update User.js

--- express/models/User.js
+++ express/models/User.js
@@ -1,29 +1,41 @@
-'use strict';
+ 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
+    // 自動主鍵 id
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    // Email
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
     },
+    // 使用者名稱
     userName: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
     },
+    // bcrypt 雜湊後的密碼
     password: {
       type: DataTypes.STRING,
       allowNull: false
     },
+    // 角色
     role: {
       type: DataTypes.STRING,
       defaultValue: 'user'
     },
+    // 計畫 (free / BASIC / 其他)
     plan: {
       type: DataTypes.STRING,
       defaultValue: 'free'
     },
+    // 如果有需要紀錄上傳次數
     uploadImages: {
       type: DataTypes.INTEGER,
       defaultValue: 0
@@ -32,14 +44,17 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       defaultValue: 0
     },
+    // 序號 (如需)
     serialNumber: {
       type: DataTypes.STRING,
       allowNull: true
     },
+    // 社群 / 電商欄位 JSON
     socialBinding: {
       type: DataTypes.STRING,
       allowNull: true
     },
+    // 是否付費
     isPaid: {
       type: DataTypes.BOOLEAN,
       defaultValue: false
@@ -49,6 +64,5 @@ module.exports = (sequelize, DataTypes) => {
     timestamps: true // createdAt, updatedAt
   });
 
-  // 若有 hooks: { beforeCreate: ... }，可加
   return User;
 };
