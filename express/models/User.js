04b9a7bac19f028f90ed4d24e951ca1c9b261461
soozoohunter <zacyao88@icloud.com>
2025-04-21 12:38:41 +0800

    Update User.js

--- express/models/User.js
+++ express/models/User.js
@@ -1,41 +1,36 @@
- 'use strict';
+'use strict';
 
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    // 自動主鍵 id
     id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.INTEGER, 
       primaryKey: true,
       autoIncrement: true
     },
-    // Email
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
     },
-    // 使用者名稱
     userName: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
     },
-    // bcrypt 雜湊後的密碼
+    // bcrypt 雜湊後
     password: {
       type: DataTypes.STRING,
       allowNull: false
     },
-    // 角色
     role: {
       type: DataTypes.STRING,
       defaultValue: 'user'
     },
-    // 計畫 (free / BASIC / 其他)
     plan: {
       type: DataTypes.STRING,
       defaultValue: 'free'
     },
-    // 如果有需要紀錄上傳次數
+    // 上傳次數
     uploadImages: {
       type: DataTypes.INTEGER,
       defaultValue: 0
@@ -44,24 +39,20 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.INTEGER,
       defaultValue: 0
     },
-    // 序號 (如需)
     serialNumber: {
-      type: DataTypes.STRING,
-      allowNull: true
+      type: DataTypes.STRING
     },
-    // 社群 / 電商欄位 JSON
+    // JSON 欄位
     socialBinding: {
-      type: DataTypes.STRING,
-      allowNull: true
+      type: DataTypes.STRING
     },
-    // 是否付費
     isPaid: {
       type: DataTypes.BOOLEAN,
       defaultValue: false
     }
   }, {
     tableName: 'users',
-    timestamps: true // createdAt, updatedAt
+    timestamps: true
   });
 
   return User;
