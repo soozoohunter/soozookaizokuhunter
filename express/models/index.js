5b05f238b436ef26d70b4009a9ffd6a4debe421b
soozoohunter <zacyao88@icloud.com>
2025-04-21 12:46:18 +0800

    Update index.js

--- express/models/index.js
+++ express/models/index.js
@@ -3,7 +3,6 @@ const path = require('path');
 require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') });
 const { Sequelize, DataTypes } = require('sequelize');
 
-// 取 DB_URL
 const DB_URL =
   process.env.DATABASE_URL ||
   'postgresql://suzoo:KaiShieldDbPass2023!@suzoo_postgres:5432/suzoo';
@@ -13,15 +12,12 @@ const sequelize = new Sequelize(DB_URL, {
   logging: false
 });
 
+// 載入 Model
 const User = require('./User')(sequelize, DataTypes);
 const File = require('./File')(sequelize, DataTypes);
 
-// 建立關聯
-User.hasMany(File, { foreignKey: 'user_id', as: 'files' });
-File.belongsTo(User, { foreignKey: 'user_id', as: 'owner' });
+// 關聯
+User.hasMany(File, { foreignKey:'user_id', as:'files' });
+File.belongsTo(User, { foreignKey:'user_id', as:'owner' });
 
-module.exports = {
-  sequelize,
-  User,
-  File
-};
+module.exports = { sequelize, User, File };
