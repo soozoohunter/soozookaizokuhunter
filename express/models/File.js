95ae4954713ea6469972f2ba7d1f330ced014bbd
soozoohunter <zacyao88@icloud.com>
2025-04-21 12:46:50 +0800

    Update File.js

--- express/models/File.js
+++ express/models/File.js
@@ -2,28 +2,14 @@
 
 module.exports = (sequelize, DataTypes) => {
   const File = sequelize.define('File', {
-    filename: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    fingerprint: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true
-    },
-    ipfs_hash: {
-      type: DataTypes.STRING
-    },
-    tx_hash: {
-      type: DataTypes.STRING
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    }
+    filename: { type:DataTypes.STRING, allowNull:false },
+    fingerprint: { type:DataTypes.STRING, allowNull:false, unique:true },
+    ipfs_hash: { type:DataTypes.STRING },
+    tx_hash: { type:DataTypes.STRING },
+    user_id: { type:DataTypes.INTEGER, allowNull:false }
   }, {
-    tableName: 'files',
-    timestamps: true
+    tableName:'files',
+    timestamps:true
   });
 
   return File;
