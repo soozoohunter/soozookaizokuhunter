d0ef27abd99bbd2c35b4056d1fdebaf302c86baa
soozoohunter <zacyao88@icloud.com>
2025-04-23 11:22:36 +0800

    Update db.js

--- express/db.js
+++ express/db.js
@@ -1,4 +1,7 @@
-// express/db.js
+/*************************************************************
+ * express/db.js
+ * - PG Pool 連線設定，與您原本保持一致
+ *************************************************************/
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -9,12 +12,12 @@ const pool = new Pool({
   database: process.env.POSTGRES_DB || 'suzoo'
 });
 
-// 可選：測試連線
+// 測試連線
 pool.connect()
   .then(() => console.log('[Express/db.js] pg Pool connected.'))
   .catch(err => console.error('[Express/db.js] pg Pool error:', err));
 
-// 匯出 query 函式以供路由使用
+// 匯出 query 供路由使用
 module.exports = {
   query: (text, params) => pool.query(text, params),
 };
