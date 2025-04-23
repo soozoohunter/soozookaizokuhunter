d43eb460a11a22b9ff4b15c625e7d6f5bd937316
soozoohunter <zacyao88@icloud.com>
2025-04-23 11:33:17 +0800

    Update authRoutes.js

--- express/routes/authRoutes.js
+++ express/routes/authRoutes.js
@@ -1,8 +1,12 @@
+/*************************************************************
+ * express/routes/authRoutes.js
+ * - 分層: 呼叫 controllers/authController 的 register & login
+ *************************************************************/
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
-// 註冊新帳號
+// 使用者註冊
 router.post('/register', authController.register);
 
 // 使用者登入
