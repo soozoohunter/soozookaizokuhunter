868faa2c1790dc428738972e9384df1c89922fef
soozoohunter <zacyao88@icloud.com>
2025-04-24 12:43:09 +0800

    Update Payment.js

--- express/models/Payment.js
+++ express/models/Payment.js
@@ -1,37 +1,38 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../db');
 
-const Payment = sequelize.define('Payment', {
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  userId: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  lastFive: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  amount: {
-    type: DataTypes.INTEGER,
-    allowNull: false
-  },
-  planWanted: {
-    type: DataTypes.ENUM('BASIC','PRO','ENTERPRISE'),
-    allowNull: false,
-    defaultValue: 'PRO'
-  },
-  status: {
-    type: DataTypes.ENUM('PENDING','APPROVED','REJECTED'),
-    allowNull: false,
-    defaultValue: 'PENDING'
-  }
-}, {
-  tableName: 'Payments',
-  timestamps: true
-});
-
-module.exports = Payment;
+module.exports = (sequelize) => {
+  const Payment = sequelize.define('Payment', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: true
+    },
+    lastFive: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    amount: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    },
+    planWanted: {
+      type: DataTypes.ENUM('BASIC','PRO','ENTERPRISE'),
+      allowNull: false,
+      defaultValue: 'PRO'
+    },
+    status: {
+      type: DataTypes.ENUM('PENDING','APPROVED','REJECTED'),
+      allowNull: false,
+      defaultValue: 'PENDING'
+    }
+  }, {
+    tableName: 'Payments',
+    timestamps: true
+  });
+  return Payment;
+};
