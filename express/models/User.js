c6d3e9f787ab36ec5c55723d696de8fda1d52ee2
soozoohunter <zacyao88@icloud.com>
2025-04-24 22:29:41 +0800

    Update User.js

--- express/models/User.js
+++ express/models/User.js
@@ -1,47 +1,36 @@
-'use strict';
-
 /************************************************************
  * models/User.js
- * - 調整 schema: username, serialNumber 改可 null
- * - 若要必填/唯一可自行改 allowNull / unique
+ * - username / serialNumber 改可為 null
  ************************************************************/
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    // 自動ID
     id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true
     },
-
-    // ★ serialNumber 預設可為 null
-    //   若您想強制必填 + 唯一，可改 allowNull: false, unique: true
+    // username 可允許為 null，若您堅持必填可改 false
+    username: {
+      type: DataTypes.STRING,
+      allowNull: true,
+      unique: false
+    },
+    // serialNumber 改可為 null
     serialNumber: {
       type: DataTypes.STRING,
-      allowNull: true,   // ← 調整為可為 null
-      unique: false      // ← 視需求可改 true
+      allowNull: true
     },
-
-    // email必填且唯一
+    // email 必填且唯一
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
     },
-
-    // ★ username 改 allowNull: true, 預計用 phone 帶入
-    username: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      unique: false      // 若您想讓 username 唯一，可改 true
-    },
-
     // 密碼
     password: {
       type: DataTypes.STRING,
       allowNull: false
     },
-
     // 角色
     role: {
       type: DataTypes.STRING,
@@ -49,13 +38,13 @@ module.exports = (sequelize, DataTypes) => {
       defaultValue: 'user'
     },
 
-    // 社群平台
+    // 社群平台 (若您保留)
     IG: { type: DataTypes.STRING, allowNull: true },
     FB: { type: DataTypes.STRING, allowNull: true },
     YouTube: { type: DataTypes.STRING, allowNull: true },
     TikTok: { type: DataTypes.STRING, allowNull: true },
 
-    // 電商平台
+    // 電商平台 (若您保留)
     Shopee: { type: DataTypes.STRING, allowNull: true },
     Ruten: { type: DataTypes.STRING, allowNull: true },
     Yahoo: { type: DataTypes.STRING, allowNull: true },
@@ -65,13 +54,12 @@ module.exports = (sequelize, DataTypes) => {
 
     // 其他欄位
     realName: { type: DataTypes.STRING, allowNull: true },
-    birthDate: { type: DataTypes.DATEONLY, allowNull: true },
+    birthDate: { type: DataTypes.STRING, allowNull: true },
     phone: { type: DataTypes.STRING, allowNull: true },
     address: { type: DataTypes.STRING, allowNull: true },
     plan: { type: DataTypes.STRING, allowNull: false, defaultValue: 'freeTrial' },
     uploadVideos: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
-    uploadImages: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
-
+    uploadImages: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 }
   }, {
     tableName: 'users',
     timestamps: true
