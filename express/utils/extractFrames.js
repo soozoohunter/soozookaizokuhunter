191531f456e47ab8f4077ac097c6ade7126f31f7
soozoohunter <zacyao88@icloud.com>
2025-04-27 11:59:29 +0800

    Update extractFrames.js

--- express/utils/extractFrames.js
+++ express/utils/extractFrames.js
@@ -4,9 +4,11 @@ const path = require('path');
 const { spawn } = require('child_process');
 
 /**
- * extractKeyFrames(videoPath, outDir, frameCount=10)
- * - 使用 ffmpeg，每秒 ~5fps，最多擷取 frameCount 張
- * - 回傳 Promise => 已排序好的 JPG 檔案路徑清單
+ * 使用 ffmpeg 擷取關鍵影格
+ * @param {string} videoPath - 本地影片檔路徑
+ * @param {string} outDir - 輸出影格的資料夾
+ * @param {number} frameCount - 最多擷取多少張影格 (預設10)
+ * @returns {Promise<string[]>} 已排序好的 JPG 檔案路徑清單
  */
 async function extractKeyFrames(videoPath, outDir, frameCount = 10) {
   if (!fs.existsSync(outDir)) {
@@ -15,8 +17,8 @@ async function extractKeyFrames(videoPath, outDir, frameCount = 10) {
   return new Promise((resolve, reject) => {
     const args = [
       '-i', videoPath,
-      '-vf', 'fps=5',
-      '-vframes', `${frameCount}`,
+      '-vf', 'fps=5',                  // 每秒 ~5fps
+      '-vframes', `${frameCount}`,     // 最多擷取多少張
       path.join(outDir, 'frame_%05d.jpg')
     ];
     const ff = spawn('ffmpeg', args);
