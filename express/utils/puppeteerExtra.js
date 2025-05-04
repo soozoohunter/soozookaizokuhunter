c38606ed9039f9f6648eba3db78ed1a81abe3b7e
soozoohunter <zacyao88@icloud.com>
2025-05-04 19:40:51 +0800

    Update puppeteerExtra.js

--- express/utils/puppeteerExtra.js
+++ express/utils/puppeteerExtra.js
@@ -1,8 +1,13 @@
-// express/utils/puppeteerExtra.js
+/**
+ * express/utils/puppeteerExtra.js
+ *
+ * 使用 puppeteer-extra + StealthPlugin，降低被搜尋網站的防爬機率。
+ */
+
 const puppeteerExtra = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 
-// 加入 Stealth 防偵測
+// 安裝 StealthPlugin
 puppeteerExtra.use(StealthPlugin());
 
 module.exports = puppeteerExtra;
