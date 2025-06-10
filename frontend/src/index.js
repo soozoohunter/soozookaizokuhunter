6aefb52ee07a0472cca05bdb7758bcd8a4116812
soozoohunter <zacyao88@icloud.com>
2025-06-10 17:44:55 +0800

    Add global XMLHttpRequest open validation

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { installXhrOpenValidation } from './setupXhrValidation';
+
+// Install global validation for XMLHttpRequest.open calls.
+installXhrOpenValidation();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
