5268c3584266a76f14686e3a2975b99a9558ce26
soozoohunter <zacyao88@icloud.com>
2025-07-02 23:46:16 +0800

    Add Files table migration

---
+++ express/migrations/20250630015330-create-files.js
@@ -0,0 +1,54 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Files', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      filename: Sequelize.STRING,
+      title: Sequelize.STRING,
+      keywords: Sequelize.TEXT,
+      mime_type: Sequelize.STRING,
+      fingerprint: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      ipfs_hash: Sequelize.STRING,
+      tx_hash: Sequelize.STRING,
+      status: Sequelize.STRING,
+      report_url: Sequelize.STRING,
+      resultJson: {
+        type: Sequelize.JSONB,
+        allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Files');
+  }
+};
