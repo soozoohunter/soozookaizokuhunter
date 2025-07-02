85cfaf545c4ee0347c3ae6a557dd60d0e84dfbcf
soozoohunter <zacyao88@icloud.com>
2025-07-02 10:46:27 +0800

    Add Sequelize CLI config and docs

---
+++ express/config/database.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+module.exports = {
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
+  test: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+  },
+};
