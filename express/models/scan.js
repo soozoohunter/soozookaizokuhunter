9387de6fedb47cc60ee548ec769429d8f569ef6e
soozoohunter <zacyao88@icloud.com>
2025-07-11 10:07:09 +0800

    Update scan.js

--- express/models/scan.js
+++ express/models/scan.js
@@ -1,12 +1,35 @@
-// express/models/scan.js
+// express/models/scan.js (v2.0 - 補全關聯)
 'use strict';
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
   class Scan extends Model {
     static associate(models) {
-      Scan.belongsTo(models.File, { foreignKey: 'file_id', as: 'file' });
-      Scan.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+      // 一個掃描 (Scan) 屬於一個檔案 (File)
+      Scan.belongsTo(models.File, { 
+        foreignKey: 'file_id', 
+        as: 'file' 
+      });
+
+      // 一個掃描 (Scan) 屬於一個使用者 (User)
+      Scan.belongsTo(models.User, { 
+        foreignKey: 'user_id', 
+        as: 'user' 
+      });
+
+      // [核心修正] 補上缺失的反向關聯
+      // 一個掃描 (Scan) 可以有多個 DMCA 請求
+      Scan.hasMany(models.DMCARequest, {
+        foreignKey: 'scan_id',
+        as: 'dmcaRequests'
+      });
+
+      // [核心修正] 補上缺失的反向關聯
+      // 一個掃描 (Scan) 可以有多個侵權報告
+      Scan.hasMany(models.InfringementReport, {
+        foreignKey: 'scan_id',
+        as: 'infringementReports'
+      });
     }
   }
   Scan.init({
