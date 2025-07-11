c554963865aa6821716209591576221829f8ac35
soozoohunter <zacyao88@icloud.com>
2025-07-11 10:08:54 +0800

    Update and rename Infringement.js to infringementreport.js

---
+++ express/models/infringementreport.js
@@ -0,0 +1,30 @@
+// express/models/infringementreport.js
+'use strict';
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class InfringementReport extends Model {
+    static associate(models) {
+      InfringementReport.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+      InfringementReport.belongsTo(models.Scan, { foreignKey: 'scan_id', as: 'scan' });
+      InfringementReport.hasMany(models.DMCARequest, { foreignKey: 'report_id', as: 'dmcaRequests' });
+    }
+  }
+  InfringementReport.init({
+    id: { 
+        type: DataTypes.INTEGER, 
+        primaryKey: true, 
+        autoIncrement: true 
+    },
+    user_id: DataTypes.INTEGER,
+    scan_id: DataTypes.INTEGER,
+    links_confirmed: DataTypes.JSONB, // 使用 JSONB 儲存連結陣列
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'InfringementReport',
+    tableName: 'InfringementReports',
+    underscored: true,
+  });
+  return InfringementReport;
+};
