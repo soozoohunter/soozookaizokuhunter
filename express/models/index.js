b6964f77406611315177aec0c8b6528091fe58f2
soozoohunter <zacyao88@icloud.com>
2025-07-11 10:20:21 +0800

    Update index.js

--- express/models/index.js
+++ express/models/index.js
@@ -1,8 +1,12 @@
-// express/models/index.js (v3.0 - Final, Stabilized Version)
+// express/models/index.js (v3.1 - Case-Sensitivity Hotfix)
 'use strict';
 
-const { Sequelize, DataTypes } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
 const logger = require('../utils/logger');
+
+const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/database.js')[env];
 const db = {};
@@ -14,24 +18,21 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
-// [核心修正] 移除動態讀取檔案的邏輯，改為手動、明確地載入每一個模型。
-// 這種方式雖然較為繁瑣，但極度穩定，且在出現問題時能提供清晰的錯誤堆疊。
-db.User = require('./user.js')(sequelize, DataTypes);
-db.SubscriptionPlan = require('./subscriptionplan.js')(sequelize, DataTypes);
-db.UserSubscription = require('./usersubscription.js')(sequelize, DataTypes);
-db.File = require('./file.js')(sequelize, DataTypes);
-db.Scan = require('./scan.js')(sequelize, DataTypes);
-db.UsageRecord = require('./usagerecord.js')(sequelize, DataTypes);
-db.InfringementReport = require('./infringementreport.js')(sequelize, DataTypes);
-db.DMCARequest = require('./dmcarequest.js')(sequelize, DataTypes);
+// [核心修正] 移除動態讀取，改為手動、明確地載入每一個模型，並使用正確的 PascalCase 檔名。
+db.User = require('./User.js')(sequelize, Sequelize.DataTypes);
+db.File = require('./File.js')(sequelize, Sequelize.DataTypes);
+db.Scan = require('./scan.js')(sequelize, Sequelize.DataTypes);
+db.UsageRecord = require('./usagerecord.js')(sequelize, Sequelize.DataTypes);
+db.SubscriptionPlan = require('./subscriptionplan.js')(sequelize, Sequelize.DataTypes);
+db.UserSubscription = require('./usersubscription.js')(sequelize, Sequelize.DataTypes);
+db.InfringementReport = require('./infringementreport.js')(sequelize, Sequelize.DataTypes);
+db.DMCARequest = require('./dmcarequest.js')(sequelize, Sequelize.DataTypes);
+// db.ManualReport = require('./ManualReport.js')(sequelize, Sequelize.DataTypes); // 如有此檔案，請取消註解
+// db.Payment = require('./Payment.js')(sequelize, Sequelize.DataTypes); // 如有此檔案，請取消註解
 
-// 假設您還有 ManualReport 和 Payment 模型，如果沒有請將其註解或刪除
-// db.ManualReport = require('./manualreport.js')(sequelize, DataTypes);
-// db.Payment = require('./payment.js')(sequelize, DataTypes);
 
 logger.info('[Database] All models have been loaded explicitly.');
 
-// 執行模型之間的關聯設定
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
