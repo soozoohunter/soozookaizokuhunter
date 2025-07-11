7ab40631d77ba28cbd0189b8bb709ca7734718bb
soozoohunter <zacyao88@icloud.com>
2025-07-11 10:25:16 +0800

    Update index.js

--- express/models/index.js
+++ express/models/index.js
@@ -1,4 +1,4 @@
-// express/models/index.js (v3.1 - Case-Sensitivity Hotfix)
+// express/models/index.js (v3.2 - Case-Sensitivity Final Fix)
 'use strict';
 
 const fs = require('fs');
@@ -21,15 +21,21 @@ if (config.use_env_variable) {
 // [核心修正] 移除動態讀取，改為手動、明確地載入每一個模型，並使用正確的 PascalCase 檔名。
 db.User = require('./User.js')(sequelize, Sequelize.DataTypes);
 db.File = require('./File.js')(sequelize, Sequelize.DataTypes);
-db.Scan = require('./scan.js')(sequelize, Sequelize.DataTypes);
-db.UsageRecord = require('./usagerecord.js')(sequelize, Sequelize.DataTypes);
-db.SubscriptionPlan = require('./subscriptionplan.js')(sequelize, Sequelize.DataTypes);
-db.UserSubscription = require('./usersubscription.js')(sequelize, Sequelize.DataTypes);
-db.InfringementReport = require('./infringementreport.js')(sequelize, Sequelize.DataTypes);
-db.DMCARequest = require('./dmcarequest.js')(sequelize, Sequelize.DataTypes);
-// db.ManualReport = require('./ManualReport.js')(sequelize, Sequelize.DataTypes); // 如有此檔案，請取消註解
-// db.Payment = require('./Payment.js')(sequelize, Sequelize.DataTypes); // 如有此檔案，請取消註解
+db.Scan = require('./scan.js')(sequelize, Sequelize.DataTypes); // 保持小寫，因您上次的檔案是小寫
+db.UsageRecord = require('./usagerecord.js')(sequelize, Sequelize.DataTypes); // 保持小寫
+db.SubscriptionPlan = require('./subscriptionplan.js')(sequelize, Sequelize.DataTypes); // 保持小寫
+db.UserSubscription = require('./usersubscription.js')(sequelize, Sequelize.DataTypes); // 保持小寫
+db.InfringementReport = require('./infringementreport.js')(sequelize, Sequelize.DataTypes); // 保持小寫
+db.DMCARequest = require('./dmcarequest.js')(sequelize, Sequelize.DataTypes); // 保持小寫
 
+// 假設您還有 ManualReport 和 Payment 模型，如果沒有請將其註解或刪除
+// 注意檔名的大小寫
+if (fs.existsSync(path.join(__dirname, 'ManualReport.js'))) {
+    db.ManualReport = require('./ManualReport.js')(sequelize, Sequelize.DataTypes);
+}
+if (fs.existsSync(path.join(__dirname, 'Payment.js'))) {
+    db.Payment = require('./Payment.js')(sequelize, Sequelize.DataTypes);
+}
 
 logger.info('[Database] All models have been loaded explicitly.');
 
